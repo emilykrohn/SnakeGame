a6fa486f8684da4e6ceaae2f6a300152a30048e0
Emily Krohn <emilykrohn2@gmail.com>
2024-06-30 19:14:37 -0700

    Snake Move Left Every Half Second
    The snake moves 50 pixels to the left every 0.5 seconds

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -1,11 +1,27 @@
 #include "raylib.h"
+#include <vector>
+
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+const int blockSize = 50;
+
+class Snake
+{
+public:
+	Vector2 frontPosition;
+};
+
+Snake snake;
+
+void InitGame();
+void DrawGame();
 
 int main(void)
 {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
-
 	InitWindow(screenWidth, screenHeight, "Snake Game");
+
+	InitGame();
 
 	SetTargetFPS(60);
 
@@ -13,12 +29,29 @@ int main(void)
 	{
 		BeginDrawing();
 			
-			ClearBackground(RAYWHITE);
+			DrawGame();
 
 		EndDrawing();
 	}
 
+
 	CloseWindow();
 
 	return 0;
+}
+
+void InitGame()
+{
+	snake.frontPosition.x = (screenWidth / 2) - (blockSize / 2);
+	snake.frontPosition.y = (screenHeight / 2) - (blockSize / 2);
+}
+
+void DrawGame()
+{
+	ClearBackground(RAYWHITE);
+
+	DrawRectangle(snake.frontPosition.x, snake.frontPosition.y, blockSize, blockSize, LIGHTGRAY);
+	snake.frontPosition.x += 50;
+
+	WaitTime(0.5);
 }
